f6b89734e7866f12a0c246b24f9e2e3944df776c
Steven <steven.james.correia@gmail.com>
2018-03-16 14:11:36 +0100

    Initial commit, basic structure

---
+++ pc5.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#
+# Author: Steven Correia
+# github: sjcorreia
+# Python Challenge #5
+
+from __future__ import print_function
+import pickle
+
+if __name__ == '__main__':
+    print("Hello World!")
